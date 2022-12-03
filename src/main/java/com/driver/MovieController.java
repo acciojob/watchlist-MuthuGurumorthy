f5fd09a968f881a8e5f7c193e1c5a9cb9b8f0905
MuthuGurumorthy <88612046+MuthuGurumorthy@users.noreply.github.com>
2022-12-03 19:24:00 +0530

    m

--- src/main/java/com/driver/MovieController.java
+++ src/main/java/com/driver/MovieController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -25,6 +26,9 @@ public class MovieController {
         return new ResponseEntity<>("Director added succesfully",HttpStatus.CREATED);
     }
 
+    public ResponseEntity<List<String>> findAllMovies(){
+        return new ResponseEntity<>(movieService.allMovies(),HttpStatus.CREATED);
+    }
     @GetMapping("/get-movie-by-name/{movieName}")
     public ResponseEntity<Movie> getMovieByName(@PathVariable String movieName){
         Movie movie = movieService.findMovie(movieName);
